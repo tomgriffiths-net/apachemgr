06947a92691ba33cfdb29879eddf60fbf59f856d
Tom <tomgriffiths2007@gmail.com>
2025-09-17 22:52:59 +0100

    v2

--- main.php
+++ main.php
@@ -1,5 +1,7 @@
 <?php
 class apachemgr{
+    private static $procs = [];
+
     public static function newServer(string $documentRoot, string $name, string|false $serverRoot=false):int|false{
         if(!is_dir($documentRoot)){
             if(!mkdir($documentRoot, 0777, true)){
@@ -46,11 +48,22 @@ class apachemgr{
         return $serverNumber;
     }
 
-    public static function start(int $serverNumber):mixed{
+    public static function start(int $serverNumber):bool{
         $info = settings::read('servers/' . $serverNumber);
         if(!is_array($info) || !isset($info['root'])){
             mklog(2, 'Failed to get info for server ' . $serverNumber);
             return false;
+        }
+
+        $port = self::getServerPort($serverNumber);
+        if(is_int($port)){
+            if(network::ping('127.0.0.1', $port)){
+                mklog(2, 'Cannot start server ' . $serverNumber . ' as port ' . $port . ' is already in use');
+                return false;
+            }
+        }
+        else{
+            mklog(1, 'Unable to get server ' . $serverNumber . ' port, unable to check port availability');
         }
 
         if(!is_file($info['root'] . '\\bin\\httpd.exe')){
@@ -58,58 +71,75 @@ class apachemgr{
             return false;
         }
 
-        $proc = proc_open('httpd.exe -D FOREGROUND', [], $pipes, realpath($info['root'] . '\\bin'));
+        if(isset(self::$procs[$serverNumber])){
+            mklog(2, 'Apache server ' . $serverNumber . ' already has an open process');
+            return false;
+        }
 
-        if(!is_resource($proc)){
+        self::$procs[$serverNumber] = proc_open(realpath($info['root'] . '\\bin\\httpd.exe'), [], $pipes, realpath($info['root']), null, ['bypass_shell'=>true]);
+
+        if(!is_resource(self::$procs[$serverNumber])){
             mklog(2, 'Failed to start httpd.exe process');
             return false;
         }
 
-        sleep(2);
-
-        $status = proc_get_status($proc);
+        $status = proc_get_status(self::$procs[$serverNumber]);
 
         if(!$status['running']){
             mklog(2, 'Apache has unexpextedly exited with code ' . $status['exitcode']);
-            return $status['exitcode'];
+            return false;
         }
 
-        return $proc;
+        return true;
     }
-    public static function stop(mixed $proc):int|false{
-        if(!is_resource($proc)){
-            mklog(2, 'Passed proc is not a resource');
+    public static function getServerProc(int $serverNumber):mixed{
+        return isset(self::$procs[$serverNumber]) ? self::$procs[$serverNumber] : false;
+    }
+    public static function stop(int $serverNumber):int|false{
+        if(!isset(self::$procs[$serverNumber])){
+            mklog(2, 'Server number ' . $serverNumber . ' is not running or was not started by this php session');
             return false;
         }
 
-        $status = proc_get_status($proc);
+        $pipe = '\\\\.\\pipe\\apachemgr_server_' . $serverNumber;
 
-        if($status['command'] !== 'httpd.exe'){
-            mklog(2, 'The passed proc resource was not for apache');
+        $fp = @fopen($pipe, 'wb');
+        if($fp === false){
+            mklog(2, 'Failed to connect to named pipe for ' . $serverNumber);
             return false;
         }
-
-        if(!$status['running']){
-            mklog(2, 'Cannot stop process that is already stopped');
+        if(!fwrite($fp, "stop\n")){
+            mklog(2, 'Failed to write to named pipe for ' . $serverNumber);
             return false;
         }
+        @fclose($fp);
+        
+        mklog(1, 'Waiting for apache to shut down gracefully');
+        $tries = 0;
+        while(true){
+            if($tries > 10){
+                mklog(2, 'Killing apache server ' . $serverNumber . ' as it has not shut down after 10 seconds');
+                if(!proc_terminate(self::$procs[$serverNumber])){
+                    mklog(2, 'Failed to send terminate signal to apache server');
+                    return false;
+                }
+            }
 
-        if(!proc_terminate($proc)){
-            mklog(2, 'Failed to send terminate signal to apache server');
-            return false;
+            if(!proc_get_status(self::$procs[$serverNumber])['running']){
+                break;
+            }
+
+            sleep(1);
         }
 
-        return proc_close($proc);
+        $exit = proc_close(self::$procs[$serverNumber]);
+
+        unset(self::$procs[$serverNumber]);
+
+        return $exit;
     }
 
     public static function setServerListen(string|int $identifier, string $listen):bool{
-        if(is_int($identifier)){
-            $identifier = self::getServerRoot($identifier);
-            if(!is_string($identifier)){
-                return false;
-            }
-        }
-
         if(!preg_match('/^[0-9.:]+$/', $listen)){
             return false;
         }
@@ -117,26 +147,12 @@ class apachemgr{
         return self::setConfDirective($identifier, "Listen", $listen);
     }
     public static function setServerRoot(string|int $identifier, string $serverRoot):bool{
-        if(is_int($identifier)){
-            $identifier = self::getServerRoot($identifier);
-            if(!is_string($identifier)){
-                return false;
-            }
-        }
-
         $serverRoot = str_replace("\\", "/", $serverRoot);
         $serverRoot = rtrim($serverRoot, "/");
 
         return self::setConfDirective($identifier, "Define SRVROOT", "\"" . $serverRoot . "\"");
     }
     public static function setServerDocRoot(string|int $identifier, string $docDir):bool{
-        if(is_int($identifier)){
-            $identifier = self::getServerRoot($identifier);
-            if(!is_string($identifier)){
-                return false;
-            }
-        }
-
         $docDir = str_replace("\\", "/", $docDir);
         $docDir = rtrim($docDir, "/");
         files::ensureFolder($docDir);
@@ -144,13 +160,6 @@ class apachemgr{
         return self::setConfDirective($identifier, "Define DOCROOT", "\"" . $docDir . "\"");
     }
     public static function setServerPhpRoot(string|int $identifier, string $phpDir):bool{
-        if(is_int($identifier)){
-            $identifier = self::getServerRoot($identifier);
-            if(!is_string($identifier)){
-                return false;
-            }
-        }
-
         $phpDir = str_replace("\\", "/", $phpDir);
         $phpDir = rtrim($phpDir, "/");
 
@@ -162,6 +171,7 @@ class apachemgr{
             if(!is_string($identifier)){
                 return false;
             }
+            $identifier .= "\\conf\\httpd.conf";
         }
 
         $lines = file($identifier);
@@ -223,6 +233,7 @@ class apachemgr{
             if(!is_string($identifier)){
                 return false;
             }
+            $identifier .= "\\conf\\httpd.conf";
         }
 
         $lines = file($identifier);
@@ -321,6 +332,28 @@ class apachemgr{
         }
         return $info['root'];
     }
+    public static function getServerPort(int $serverNumber):int|false{
+        $root = self::getServerRoot($serverNumber);
+        if(!is_string($root)){
+            mklog(2, 'Failed to get server root for server ' . $serverNumber);
+            return false;
+        }
+
+        $line = self::getLineBeginingWith('Listen',  $root . "\\conf\\httpd.conf");
+
+        if(!is_string($line)){
+            mklog(2, 'Failed to find server listen in httpd.conf for server ' . $serverNumber);
+            return false;
+        }
+
+        $dots = strripos($line, ':');
+        if($dots){
+            return intval(substr($line, $dots+1));
+        }
+        else{
+            return intval(trim(substr($line, 7)));
+        }
+    }
     
     private static function replaceLineBeginingWith(string $starting, string $replacement, array $lines):array|false{
         $count = strlen($starting);
@@ -344,8 +377,34 @@ class apachemgr{
 
         return $lines;
     }
+    private static function getLineBeginingWith(string $starting, string $file, bool $ignoreCase=true):string|false{
+        $lines = file($file);
+        if(!is_array($lines)){
+            return false;
+        }
+
+        if($ignoreCase){
+            $starting = strtolower($starting);
+        }
+
+        $startLen = strlen($starting);
+
+        foreach($lines as $line){
+            $line = trim($line);
+
+            if($ignoreCase){
+                $line = strtolower($line);
+            }
+
+            if(substr($line, 0, $startLen) === $starting){
+                return $line;
+            }
+        }
+
+        return false;
+    }
     private static function downloadApache(string $serverRoot):bool{
-        if(!downloader::downloadFile('https://files.tomgriffiths.net/php-cli/files/httpd-2.4.65-250724-Win64-VS17-CustomConfV1.zip','temp/apachemgr/apache.zip')){
+        if(!downloader::downloadFile('https://files.tomgriffiths.net/php-cli/files/httpd-2.4.65-250724-Win64-VS17-CustomConfAndModuleV2.zip','temp/apachemgr/apache.zip')){
             mklog(2,'Failed to download apache with custom config');
             return false;
         }
